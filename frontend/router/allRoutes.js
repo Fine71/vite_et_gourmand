733a6e356b066d059c2160ed92682aefba135c53
Delphine Esparon <delphine.esparon@gmail.com>
2026-07-08 12:45:46 +0200

    créations pages menu

--- frontend/router/allRoutes.js
+++ frontend/router/allRoutes.js
@@ -4,6 +4,9 @@ import Route from "./route.js";
 
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html", []),
+    new Route("/menus", "Menus", "./pages/menus.html", []),
+    new Route("/details", "Détails du menu", "./pages/menus/details.html", []),
+    new Route("/commander", "Ma commande", "./pages/menus/commander.html", []),
 ]
 
 // Le titre s'affiche comme ceci : Route.titre - websiteName
